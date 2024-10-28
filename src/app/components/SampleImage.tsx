749127e7eeec0f84ed6918e21515559132d9af42
조우식 <chousik01@gmail.com>
2024-10-28 16:20:35 +0900

    style | sampleimage.tsx

--- src/app/components/SampleImage.tsx
+++ src/app/components/SampleImage.tsx
@@ -34,14 +34,18 @@ export default function SampleImage({ sampleImages }: SampleImageProps) {
       {/* 반응형 및 그리드 배치 */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pb-10 justify-items-center">
         {sampleImages.map((element, index) => (
-          <div key={index} onClick={() => openModal(element)} className="cursor-pointer">
+          <div
+            key={index}
+            onClick={() => openModal(element)}
+            className="relative cursor-pointer w-full h-40 overflow-hidden rounded-lg"
+          >
             <Image
               src={element}
               alt="이미지입니다!"
-              width={300}
-              height={400}
-              className="w-auto h-auto"
+              layout="fill" // 부모 요소를 가득 채우도록 설정
+              objectFit="cover" // 크기에 맞춰 이미지를 자름
               priority
+              className="rounded-lg"
             />
           </div>
         ))}
@@ -53,7 +57,7 @@ export default function SampleImage({ sampleImages }: SampleImageProps) {
           className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
           onClick={handleBackgroundClick}
         >
-          <div className="relative">
+          <div className="relative max-w-full max-h-full p-4">
             {/* 닫기 버튼 */}
             <button
               className="absolute top-2 right-2 text-white text-2xl font-bold"
@@ -66,9 +70,11 @@ export default function SampleImage({ sampleImages }: SampleImageProps) {
               <Image
                 src={selectedImage}
                 alt="원본 이미지입니다!"
-                width={800}
-                height={1000}
-                className="w-auto h-auto"
+                layout="responsive"
+                objectFit="contain" // 원본 비율 유지
+                width={800} // 최대 너비, 고정값 아님
+                height={1000} // 최대 높이, 고정값 아님
+                className="max-w-full max-h-[90vh]" // 화면의 최대 크기 제어
               />
             )}
           </div>
