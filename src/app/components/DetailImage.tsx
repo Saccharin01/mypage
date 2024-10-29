baca4588b3194aa86af79a42488f32835586b5b0
Saccharin01 <chousik01@gmail.com>
2024-10-29 10:55:33 +0900

    feat | DetailImage.tsx
    상세 페이지에서 사용하는 컴포넌트
    이미지와 타이틀, 페럼스 값을 가지며 해당 컴포넌트에 모달 창이 추가되어있음

--- src/app/components/DetailImage.tsx
+++ src/app/components/DetailImage.tsx
@@ -1,26 +1,65 @@
-"use client"
+"use client";
 
 import Image from "next/image";
-import { useLiteral } from "./context/useLiteral";
+import { ISampleImage } from "@/shared/interface/static.interface";
+import { useModal } from "./hooks/useModal";
 
-
-export default function DetailImage() {
+export default function DetailImage({ sampleImages }: { sampleImages: ISampleImage[] }) {
   
-  
-  const data = useLiteral()
-
-
+  const { isModalOpen, selectedImage, openModal, closeModal, handleBackgroundClick } = useModal();
 
   return (
     <div>
-      <div>
-        <Image
-        src={""}
-        alt=""/>
+      <h1 className="text-3xl font-bold mb-4">Sample Image</h1>
+      <div className="grid grid-cols-1 gap-4 pb-10">
+        {sampleImages.map((element, index) => (
+          <div key={index} className="flex">
+            <div
+              className="relative cursor-pointer w-1/3 h-40 overflow-hidden rounded-lg"
+              onClick={() => openModal(element.img)}
+            >
+              <Image
+                src={element.img}
+                alt="이미지입니다!"
+                fill
+                sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+                className="object-cover rounded-lg"
+                priority
+              />
+            </div>
+            <div className="ml-6">
+              <h1 className="text-3xl font-bold">{element.title}</h1>
+              <p>{element.params}</p>
+            </div>
+          </div>
+        ))}
+        {isModalOpen && (
+          <div
+            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
+            onClick={handleBackgroundClick}
+          >
+            <div className="relative max-w-full max-h-full p-4">
+              <button
+                className="absolute top-2 right-2 text-white text-2xl font-bold"
+                onClick={closeModal}
+              >
+                &times;
+              </button>
+              {selectedImage && (
+                <Image
+                  src={selectedImage}
+                  alt="원본 이미지입니다!"
+                  sizes="100vw"
+                  width={800}
+                  height={1000}
+                  style={{ width: "auto", height: "auto" }}
+                  className="object-contain max-w-full max-h-[90vh]"
+                />
+              )}
+            </div>
+          </div>
+        )}
       </div>
-
-      <div></div>
     </div>
-
   );
 }
