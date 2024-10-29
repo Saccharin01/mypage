ce61a6ba06d247d8510ede305ff62cd103b50527
Saccharin01 <chousik01@gmail.com>
2024-10-29 10:54:40 +0900

    feat | ButtonGroup.tsx
    버튼 그룸 컴포넌트에서 사용하는 인터페이스를 컨벤션에 맞춰 조정

--- src/app/components/ButtonGroup.tsx
+++ src/app/components/ButtonGroup.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { IstaticData } from "@/shared/interface/static.interface";
+import { ILinkbuttons } from "@/shared/interface/static.interface"
 
-export default function ButtonGroup({ aboutProps }: { aboutProps: IstaticData['details']['about'] }) {
+export default function ButtonGroup({ aboutProps }: { aboutProps: ILinkbuttons[] }) {
   return (
     <div className="flex flex-wrap gap-4 my-6">
       {aboutProps.map((button, index) => (
