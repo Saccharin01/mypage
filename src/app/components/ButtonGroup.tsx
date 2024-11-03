2b0259a68c411bb2298ff9cfa0233bf782fbdbe2
Saccharin01 <chousik01@gmail.com>
2024-11-04 03:01:59 +0900

    style | ButtonGroup.tsx
    버튼 컴포넌트의 색상을 변경. 너무.. 어디서 퍼 온 것 처럼 생겼잖아..

--- src/app/components/ButtonGroup.tsx
+++ src/app/components/ButtonGroup.tsx
@@ -10,7 +10,7 @@ export default function ButtonGroup({ aboutProps }: { aboutProps: ILinkbuttons[]
           href={button.link}
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition"
+          className="bg-[#a76767] text-white py-2 px-4 rounded-md hover:bg-[#e06969] focus:outline-none focus:ring-2 focus:ring-blue-300 transition"
         >
           {button.placeholder}
         </Link>
