ecb5c58b45529c2e369fa87908ba1e1110d96509
saccharin01 <chousik01@gmail.com>
2025-03-17 17:14:18 +0900

    feat | page.tsx "/"
    메인 페이지의 레이아웃 수정 및 신규 모듈을 이용한 애니메이션 효과 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,47 @@
 "use client";
+
+import { motion } from "framer-motion";
 import ProjectElement from "./components/ProjectElement";
 import StackElement from "./components/StackElement";
 
 export default function MainPage() {
   return (
-    <>
-      <header className="mb-6 pt-6">
-        <div className="max-w-4xl">
-          <h1 className="text-4xl font-bold">
-          요리하다가 개발하게 된 조우식입니다.
-          </h1>
-        </div>
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      {/* 랜딩 메시지 */}
+      <header className="flex flex-col justify-center items-center h-[100vh]">
+        <motion.h1
+          className="text-5xl font-bold mb-3"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0 }}
+        >
+          반갑습니다
+        </motion.h1>
+        <motion.h1
+          className="text-2xl font-bold mb-3"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.8 }}
+        >
+          Was a Cook, Now i develop
+        </motion.h1>
+        <motion.h1
+          className="text-4xl font-bold"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 1.6 }}
+        >
+          조우식입니다.
+        </motion.h1>
       </header>
-      <main>
+
+      <main className="mt-20 w-full">
         <StackElement />
       </main>
-      <footer className="mt-10 pb-5">
+
+      <footer className="mt-10 pb-5 w-full">
         <ProjectElement />
       </footer>
-    </>
+    </div>
   );
 }
