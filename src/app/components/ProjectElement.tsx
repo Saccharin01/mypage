25e7dafd79d2f2ab7c6baa12f0cad64291ab3b20
saccharin01 <chousik01@gmail.com>
2025-04-17 18:01:35 +0900

    feat | ProjectElement.tsx
    정적 데이터 길이가 늘어남에 따라 랜더링이 진행되지 않던 부분 수정

--- src/app/components/ProjectElement.tsx
+++ src/app/components/ProjectElement.tsx
@@ -43,11 +43,12 @@ export default function ProjectElement() {
       <div className="grid grid-cols-1 gap-8">
         {staticData.map((element, index) => (
           <motion.div
-            key={index}
-            initial={{ opacity: 0, y: 50 }}
-            animate={isVisible ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.6, delay: index * 0.2 }}
-          >
+          key={element.title+index}
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true, amount: 0.3 }}
+          transition={{ duration: 0.6, delay: index * 0.2 }}
+        >
             <Link href="/details">
               <div
                 className={`flex flex-col md:flex-row ${
